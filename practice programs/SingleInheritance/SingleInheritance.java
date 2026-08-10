54b27be2aa6fa7582d8957189677d815ad2c070c
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-08-10 09:16:20 +0530

    Add SingleInheritance.java with two classes
    Implement Single Inheritance with two classes and methods to display sums.

---
+++ practice programs/SingleInheritance/SingleInheritance.java
@@ -0,0 +1,23 @@
+package javaprograms;
+class First{
+    int a=2;
+    void sum()
+    {
+        System.out.println("sum is:"+(a+5));
+    }
+}
+class Second1 extends First {
+    int b=3;
+    void display(){
+
+        System.out.println("sum is:"+(b+8));
+    }
+
+    public static void main() {
+        Second1 s=new Second1();
+        s.display();
+        s.sum();
+        System.out.println("variable is:"+s.a);
+
+    }
+}
