43bdd59b83b519089c1d423a4d9ad256f6c472b5
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-08-10 08:56:13 +0530

    Create AssignValue.java

---
+++ practice programs/AssignValues/AssignValue.java
@@ -0,0 +1,13 @@
+package javaprograms;
+import java.util.Scanner;
+public class AssignValue {
+    public static void main(String[] args) {
+        System.out.println("enter two integers");
+        Scanner S=new Scanner(System.in);
+        int c=S.nextInt();
+        int h=S.nextInt();
+        System.out.println("addition is"+(c+h));
+
+
+    }
+}
