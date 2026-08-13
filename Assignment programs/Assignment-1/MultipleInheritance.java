c54d127d9f71646d6954dfdc5960b5cf2827ebb6
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-08-13 22:11:47 +0530

    Create MultipleInheritance.java

---
+++ Assignment programs/Assignment-1/MultipleInheritance.java
@@ -0,0 +1,35 @@
+package javaprograms;
+interface M1{
+    int x = 20;
+    void display1();
+}
+interface M2{
+    int y=10;
+    void display2();
+}
+public class MultipleInheritance implements M1,M2 {
+    @Override
+public void display1() {
+    System.out.println("hello");
+
+}
+
+    @Override
+    public void display2() {
+        System.out.println("bye");
+
+
+    }
+
+    static void main() {
+
+
+        MultipleInheritance m1 = new MultipleInheritance();
+        System.out.println("result is:"+ M1.x);
+        m1.display1();
+    }
+} 
+/*
+result is:20
+hello*/
+
