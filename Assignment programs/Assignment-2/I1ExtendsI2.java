3e6ad04fe8852c3df5feedf46d8beb324565a81c
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-08-30 20:37:02 +0530

    Create I1ExtendsI2.java

---
+++ Assignment programs/Assignment-2/I1ExtendsI2.java
@@ -0,0 +1,27 @@
+package javaprograms;
+
+interface I2 {
+    void add();
+}
+
+interface I1 extends I2 {
+    void sub();
+}
+
+class C1 implements I1 {
+    int a = 10, b = 5;
+@Override
+    public void add() {
+        System.out.println("Addition = " + (a + b));
+    }
+@Override
+    public void sub() {
+        System.out.println("Subtraction = " + (a - b));
+    }
+
+    public static void main(String[] args) {
+        C1 c = new C1();
+        c.add();
+        c.sub();
+    }
+}
