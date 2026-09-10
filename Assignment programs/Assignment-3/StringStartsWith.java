b47f2e6da806a9c6012eda5500cf067fd4de6991
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:40:49 +0530

    Add StringStartsWith class with main method
    Implement StringStartsWith class to check string prefix.

---
+++ Assignment programs/Assignment-3/StringStartsWith.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class StringStartsWith {
+    public static void main(String[] args) {
+        String s = "ice cream";
+
+        System.out.println("Starts with ice= " + s.startsWith("cream"));
+    }
+}
+/*output
+Starts with ice= false
+*/
