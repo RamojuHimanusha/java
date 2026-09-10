d104a931e38ad3e8e23969728ca3a60db9b3f912
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:36:58 +0530

    Create StringIsEmpty.java to check string emptiness
    Add a Java program to check if a string is empty.

---
+++ Assignment programs/Assignment-3/StringIsEmpty.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class StringIsEmpty {
+    public static void main(String[] args) {
+        String s = "java";
+
+        System.out.println("Is Empty = " + s.isEmpty());
+    }
+}
+/*output
+Is Empty = false
+ */
