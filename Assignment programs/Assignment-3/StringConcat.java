4359333c5846a72963e4f38be79d662408c06285
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:30:56 +0530

    Add StringConcat class for string concatenation
    Implement StringConcat class to concatenate two strings.

---
+++ Assignment programs/Assignment-3/StringConcat.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class StringConcat {
+        public static void main(String[] args) {
+            String s1 = "Hello ";
+            String s2 = "World";
+
+            System.out.println("Result = " + s1.concat(s2));
+        }
+    }
+    /*output
+Result = Hello World
+*/
