fd5c0b9c3250cb19acd76927245423d9c4ac4fbe
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:31:47 +0530

    Add StringContains class to check substring presence

---
+++ Assignment programs/Assignment-3/StringContains.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class StringContains {
+    public static void main(String[] args) {
+        String s = "Hello World";
+
+        System.out.println("Contains World = " + s.contains("World"));
+    }
+}
+/*output
+Contains World = true*/
