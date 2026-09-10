1b6c78ce7b8ac92e9412fff307f68d13dd449506
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:28:36 +0530

    Add StringLength program to calculate string length
    This program calculates and prints the length of a given string.

---
+++ Assignment programs/Assignment-3/StringLength.java
@@ -0,0 +1,10 @@
+package javapractice;
+
+public class StringLength {
+        public static void main(String[] args) {
+            String s1="hello";
+            System.out.println("length="+s1.length());
+        }
+    }
+/*output
+length=5*/
