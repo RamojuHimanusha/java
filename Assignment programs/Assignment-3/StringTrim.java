36d88a4f0d509ed877f0288ab6d3b79f2751de37
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:42:28 +0530

    Add StringTrim class to demonstrate string trimming

---
+++ Assignment programs/Assignment-3/StringTrim.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class StringTrim {
+    public static void main(String[] args) {
+        String s = "      Hello";
+
+        System.out.println("After trim = " + s.trim());
+    }
+}
+/*output
+After trim = Hello
+*/
