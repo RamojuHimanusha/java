510ab9f2266ef12bfd4e536f18dd20e99e07fd04
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:41:40 +0530

    Add StringSubString.java to demonstrate substring usage

---
+++ Assignment programs/Assignment-3/StringSubString.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class StringSubString {
+    public static void main(String[] args) {
+        String s = "Hello World";
+
+        System.out.println("Substring = " + s.substring(6));
+    }
+}
+/*output
+Substring = World
+ */
