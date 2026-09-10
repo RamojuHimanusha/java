41c7ca62d1bccdb34c0c33446e664b73152afa56
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:39:30 +0530

    Add StringReplace class for character replacement

---
+++ Assignment programs/Assignment-3/StringReplace.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class StringReplace {
+    public static void main(String[] args) {
+        String s = "javaprogram";
+
+        System.out.println("Replaced = " + s.replace('a', 'x'));
+    }
+}
+/*output
+Replaced = jxvxprogrxm
+*/
