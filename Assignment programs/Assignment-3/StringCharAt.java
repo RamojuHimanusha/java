0cf1d22b8131f70bef79f4e25a763a6af4c68e4c
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:29:35 +0530

    Add StringCharAt class to show character retrieval
    Implement StringCharAt class to demonstrate charAt method.

---
+++ Assignment programs/Assignment-3/StringCharAt.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class StringCharAt {
+     public static void main(String[] args) {
+         String s1="ice cream";
+         System.out.println("character="+s1.charAt(4));
+
+    }
+}
+/*output
+character=c*/
