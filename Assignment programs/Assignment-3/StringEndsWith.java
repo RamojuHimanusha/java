1bca82ede56f02d721949109040d01edca9b3c11
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:32:45 +0530

    Add StringEndsWith class to check string suffix

---
+++ Assignment programs/Assignment-3/StringEndsWith.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class StringEndsWith {
+    public static void main(String[] args) {
+        String s ="java program";
+
+        System.out.println("Ends with program = " + s.endsWith("program"));
+    }
+}
+/*output
+Ends with program = true*/
