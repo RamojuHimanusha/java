a94cfb7dfa84d982689a35ba7e23a52d5b69f6ff
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:36:02 +0530

    Add StringIndexOf.java to demonstrate indexOf method

---
+++ Assignment programs/Assignment-3/StringIndexOf.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class StringIndexOf {
+    public static void main(String[] args) {
+        String s = "Hello";
+
+        System.out.println("Index = " + s.indexOf('l'));
+    }
+}
+/*output
+Index = 2*/
