75cf36d83125cb441378a7b4fc9a028c944b380c
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:43:10 +0530

    Add StringUpperCase.java to convert string to uppercase

---
+++ Assignment programs/Assignment-3/StringUpperCase.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class StringUpperCase {
+    public static void main(String[] args) {
+        String s1="java program";
+        System.out.println("uppercase="+s1.toUpperCase());
+
+    }
+}
+/*output
+uppercase=JAVA PROGRAM*/
