b73742538123992faa28d3fa99d7e9712ee45859
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:33:39 +0530

    Add StringEquals class to compare two strings

---
+++ Assignment programs/Assignment-3/StringEquals.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class StringEquals {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        System.out.println("Equal = " + s1.equals(s2));
+    }
+}
+/*output
+Equal = true*/
