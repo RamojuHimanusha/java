39ce804a6f7fca44a974e43d947fa346b093c063
RamojuHimanusha <ramojuhimanusha.25.csm@anits.edu.in>
2026-09-10 19:38:29 +0530

    Create StringLowerCase.java

---
+++ Assignment programs/Assignment-3/StringLowerCase.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class StringLowerCase {
+    public static void main(String[] args) {
+        String s = "HELLO";
+
+        System.out.println("Lowercase = " + s.toLowerCase());
+    }
+}
+  /* output 
+           Lowercase = hello*/
